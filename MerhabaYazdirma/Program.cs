0bef61b31e90efef5b0a2aa792fd96c634515e08
berat584 <berat584@BPC08>
2023-12-05 09:13:36 +0300

    Proje dosyası ekle.

---
+++ MerhabaYazdirma/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerhabaYazdirma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("adını gir");
+            string ad =Convert.ToString(Console.ReadLine());
+            Selam selam = new Selam();
+            selam.selamla(ad);
+ 
+        }
+    }
+    public class Selam
+    {
+        public void selamla(string ad= "emre")
+        {
+            Console.WriteLine("Merhaba " + ad);
+        }
+
+    }
+}
